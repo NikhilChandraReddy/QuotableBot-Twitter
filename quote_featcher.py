0ab3164663551417741e993b7316c65b75e2c3d5
Nikhil Reddy <48628715+NikhilChandraReddy@users.noreply.github.com>
2023-08-06 01:08:59 -0400

    Update quote_featcher.py
    Add get_quotes method

--- quote_featcher.py
+++ quote_featcher.py
@@ -1,3 +1,8 @@
 class QuoteFetcher:
     def __init__(self, file_path):
         self.file_path = file_path
+               
+    def get_quotes(self):
+        with open(self.file_path, "r", encoding="utf-8") as file:
+            quotes = [line.strip() for line in file if line.strip()]
+        return quotes
