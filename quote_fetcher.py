5ea9ecf7ded4a3e8b1f4caefcdbee3d16cd79e8a
Nikhil Reddy <48628715+NikhilChandraReddy@users.noreply.github.com>
2023-08-13 03:47:26 -0400

    Use API-Ninjas quotes api

--- quote_fetcher.py
+++ quote_fetcher.py
@@ -1,14 +1,26 @@
+import requests
 import random
 
 class QuoteFetcher:
-    def __init__(self, file_path='quotes.json'):
-        self.file_path = file_path
-        self.quotes = self._read_quotes()
-    
-    def _read_quotes(self):
-        with open(self.file_path, 'r') as file:
-            data = eval(file.read())
-        return data['quotes']
-               
+    def __init__(self, category, api_key, api_url='https://api.api-ninjas.com/v1/quotes?category={}'):
+        self.api_key = api_key
+        self.api_url = api_url
+        self.category = category
+        self.quotes = self._fetch_quotes()
+
+    def _fetch_quotes(self):
+        headers = {
+            'X-Api-Key': self.api_key
+        }
+        response = requests.get(self.api_url.format(self.category)), headers=headers)
+        if response.status_code == 200:
+            data = response.json()
+            return data
+        else:
+            raise Exception(f"Error: Failed to fetch quotes. Status code: {response.status_code} \n Message: {response.text}")
+
     def get_random_quote(self):
-        return random.choice(self.quotes)
+        if self.quotes:
+            return random.choice(self.quotes)
+        else:
+            return "Error: No quotes available."
